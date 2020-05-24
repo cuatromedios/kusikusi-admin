d9b0e2b88f0910edbc35a5ca70623402127f1d2d
Ramses <ramses@cuatromedios.com>
2020-05-24 17:19:18 -0500

    Setting a new default port for admin in dev mode

--- quasar.conf.js
+++ quasar.conf.js
@@ -154,7 +154,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port: 8080,
+      port: 8001,
       open: false // opens browser window automatically
     },
 
